b0c43c37618034a5a0e2ca797aa6ab707e8116fb
Rachata Burin <rachataburintam@gmail.com>
2021-11-23 17:08:30 +0700

    handle error

--- pages/build_model.py
+++ pages/build_model.py
@@ -14,45 +14,47 @@ def app():
         ### Model Building
         """)
     dataset_name = st.sidebar.selectbox("Select Dataset",("Breast Cancer","Iris","Wine","Upload your dataset(.csv)"),index=1)
+    try:
+        x,y,feature_names,target_name,target_unique = utils.get_dataset(dataset_name)
+        df = pd.DataFrame(x,columns = feature_names)
 
-    x,y,feature_names,target_name,target_unique = utils.get_dataset(dataset_name)
-    df = pd.DataFrame(x,columns = feature_names)
+        model_name = st.sidebar.selectbox("Select classifier",("SVM","KNN","Tree"))
+        st.write(f"""
+         Your selected classifier is **{model_name }**
+         """)
 
-    model_name = st.sidebar.selectbox("Select classifier",("SVM","KNN","Tree"))
-    st.write(f"""
-     Your selected classifier is **{model_name }**
-     """)
+        selected_columns = st.multiselect("Select preferred columns", df.columns, default= list(df.columns))
+        df = df[selected_columns]
 
-    selected_columns = st.multiselect("Select preferred columns", df.columns, default= list(df.columns))
-    df = df[selected_columns]
-
-    all_columns = df.columns.to_list()
-    st.dataframe(df,800,300)
+        all_columns = df.columns.to_list()
+        st.dataframe(df,800,300)
 
 
 
-    params = utils.add_parameter(model_name)
+        params = utils.add_parameter(model_name)
 
-    if st.sidebar.button("build model"):
+        if st.sidebar.button("build model"):
 
-        model = utils.get_model(model_name,params)
+            model = utils.get_model(model_name,params)
 
-        x_train, x_test, y_train, y_test = train_test_split(df,y,test_size=0.2,random_state=0)
+            x_train, x_test, y_train, y_test = train_test_split(df,y,test_size=0.2,random_state=0)
 
-        model.fit(x_train,y_train)
+            model.fit(x_train,y_train)
 
-        y_pred = model.predict(x_test)
+            y_pred = model.predict(x_test)
 
-        score =classification_report(y_test,y_pred,output_dict=True)
-        df_score = pd.DataFrame(score).transpose()
+            score =classification_report(y_test,y_pred,output_dict=True)
+            df_score = pd.DataFrame(score).transpose()
 
-        st.write("""### Your model performance""")
-        st.dataframe(df_score.iloc[:-3])
-        st.write("""#### Your accuracy """,round(df_score.iloc[-3,0]*100,2),"%")
-        st.write("""Your test set prediction """)
-        x_test['target'] = y_test
-        x_test['predicted'] = y_pred
-        x_test['result'] = y_test == y_pred
-        st.dataframe(x_test)
-    else:
-        pass
+            st.write("""### Your model performance""")
+            st.dataframe(df_score.iloc[:-3])
+            st.write("""#### Your accuracy """,round(df_score.iloc[-3,0]*100,2),"%")
+            st.write("""Your test set prediction """)
+            x_test['target'] = y_test
+            x_test['predicted'] = y_pred
+            x_test['result'] = y_test == y_pred
+            st.dataframe(x_test)
+        else:
+            pass
+    except:
+        st.write("Something error TT")
